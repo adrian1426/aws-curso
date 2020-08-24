8ccc29f5ae2b51bcc44cef07b9f8100d6c4aedf3
Adrian1426 <adrianhh1426@gmail.com>
2020-08-23 20:02:42 -0500

    conexión a dynamoDB

--- express_dynamoDB/handler.js
+++ express_dynamoDB/handler.js
@@ -3,9 +3,20 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const aws = require('aws-sdk');
+const bodyParser = require('body-parser');
+
+const dynamoDB = new aws.DynamoDB.DocumentClient();
+
+app.use(bodyParser.json({ strict: false }));
 
 app.get('/', (req, res) => {
   res.send('Hola mundo con express');
+});
+
+app.post('/users', (req, res) => {
+  const { userId, name } = req.body;
+  res.json({ userId, name });
 });
 
 module.exports.generic = serverless(app);
